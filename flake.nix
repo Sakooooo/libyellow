8f0ea8610c468a25d3ea7a9b45155dd1546272a5
Sakooooo <78461130+Sakooooo@users.noreply.github.com>
2024-10-10 06:59:48 +0400

    flake init

---
+++ flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "flake";
+
+  inputs = { nixpkgs.url = "github:nixos/nixpkgs?ref=nixos-unstable"; };
+
+  outputs = { self, nixpkgs }:
+    let
+      inherit (nixpkgs) lib;
+      inherit (builtins) attrValues;
+      eachSystem = f:
+        lib.genAttrs [ "x86_64-linux" "aarch64-linux" ]
+        (system: f nixpkgs.legacyPackages.${system});
+    in {
+
+      # packages = eachSystem (pkgs: rec {
+      #   default = libyellow;
+      #   libyellow = pkgs.callPackage ./package.nix { };
+      # });
+
+      devShells = eachSystem (pkgs: {
+        default =
+          pkgs.mkShell { packages = attrValues { inherit (pkgs) meson; }; };
+      });
+
+    };
+}
